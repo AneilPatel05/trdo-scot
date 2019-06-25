a1816ecc0cca3ec01fcb924309465e1a84a81d1f
Aneilpatel05 <anil.patel.gty@gmail.com>
2019-06-26 00:33:41 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,45 @@
+const { Streamer } = require('steem-stream');
+
+const options = {
+    ACTIVE_KEY: '',
+    APP_NAME: 'trendotoken',
+    USERNAME: 'trendotoken'
+  }
+  
+const ss = new Streamer(options);
+const from = 'trendotoken';
+const symbol = 'TRDO';
+const memo = 'Here are your trendokens!'
+var lastrewardcycleblocknum = 0;
+const rewardcyclefrequency = 28800;
+var dailybalance = 2000;
+// Kickstart the streamer to watch the Steem blockchain
+ss.start();
+
+ss.onComment((op, blockNumber, blockId, prevBlockId, trxId, blockTime) => {
+    if(op.body ==='!trendo'){
+        // let accounts = []
+        // accounts.push({
+        //     account: op.author,
+        //     amount: '2.00'
+        // })
+        // accounts.push({
+        //     account: op.parent_author,
+        //     amount: '8.00'
+        // })
+        // ss.transferSteemEngineTokensMultiple(from, accounts , symbol, memo , amount = '0')
+        if(dailybalance >= 10){
+            ss.transferSteemEngineTokens(from, op.author  , amount = '2', symbol, memo)
+            setTimeout(function(){
+                ss.transferSteemEngineTokens(from, op.parent_author , amount = '8', symbol , memo)
+            },3500)
+
+            dailybalance -= 10
+            console.log('Sent tokens to '+op.parent_author+' and '+op.author+' dailybalance '+dailybalance);
+        } else{
+            console.log('Reward Limit Reached today, try again ');
+        }
+
+        
+    }
+});
